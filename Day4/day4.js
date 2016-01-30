656b82d3b7c24149b061bab7347c2be74f0791ad
GLapon <glapon@gmail.com>
2016-01-29 20:38:22 -0500

    Replaced dupe, did day 4 part 1

--- Day4/day4.js
+++ Day4/day4.js
@@ -3,17 +3,22 @@
 //given some string, find the smallest number that when concatenated to that string
 //makes an MD5 hash that has five 0s at the start of its hexadecimal representation
 
-var input = 'iwrupvqb';
 
-//md5 hash function
+var crypto = require('crypto');
+
+var input = 'iwrupvqb';
 
 var counter = 1;
 
 var number;
 
 while (!number) {
-	//add hash function here:
-	if (md5(input + counter).substr(0,4) == '00000') {
+	
+	var hasher = crypto.createHash('md5');
+	hasher.update(input + counter, 'ascii');
+    var hash = hasher.digest('hex');
+
+	if (hash.startsWith('00000')) { //test to see if it has 5 zeroes at beginning
 		number = counter;
 		break;
 	};
