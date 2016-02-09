b7fb2df38682c5d305a07560ba310249deffa045
GLapon <glapon@gmail.com>
2016-02-09 01:52:30 -0500

    fixed issue with day20

--- Day20/Day20.js
+++ Day20/Day20.js
@@ -4,8 +4,10 @@ let _ = require('lodash');
 
 let presentsCap = 33100000;
 
+
 let factorize = (number) => {
-    if (number < 4) { return [1, number]; }
+    if (number == 1) { return [1]; }
+    else if (number < 4) { return [1, number]; }
     else if (number == 4) {return [1, 2, 4]; };
 
     let possibles = _.range(2, Math.floor(number/2));
@@ -14,22 +16,20 @@ let factorize = (number) => {
         if ( number % element == 0) { factors.push(element, number/element); };
     }, [1,number]);
 
-    return (_.unionWith(factors, factors, _.isEqual)); // return unique factors (to speed up, could fix above so (in cases like 12) it only pushes factors once by checking if other factor is < number/2), and remove this
+    return (_.unionWith(factors, factors, _.isEqual)); //speed up by removing need? (12 as example)
 };
 
 let presentSum = (number) => {
     let factors = factorize(number);
 
-    let totalPresents = _.transform(factors, (result, factor) => {
-        result = result + factor * 10;
+    let totalPresents = _.reduce(factors, (result, factor) => {
+        return result + factor * 10;
     }, 0);
 
     return totalPresents;
 };
 
 let houseCounter = (cap) => {
-    let limit = cap;
-
     for ( let house = 1; true; house++) { // to speed this up, is there a number, based on value of cap, to start at higher than 1?
         if (presentSum(house) >= cap) { return house; };
     };
