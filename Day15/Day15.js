2ac89c21b329cd65e48ef2bdfa34eea0400a3d88
GLapon <glapon@gmail.com>
2016-03-11 20:01:26 -0500

    Working on day 8, 15, 25

---
+++ Day15/Day15.js
@@ -0,0 +1,24 @@
+
+
+"use strict";
+fs = require('fs'); //https://docs.nodejitsu.com/articles/file-system/how-to-read-files-in-nodejs
+let _ = require('lodash');
+
+let inputFile = "/Users/glapon/Desktop/Advent of Code/Day15/input.txt";
+
+let input;
+
+fs.readFile(inputFile, 'utf8', (err, data) => {
+    if (err) { return console.log(err); };
+    input = data;
+});
+
+let inputArray = input.split('\n');
+
+let toObject = (string) {
+    let properties = {};
+    properties.name = string.slice(0, string.indexOf(':'));
+    
+}
+
+let ingredients = _.transform()
---
+++ Day25/Day25.js
@@ -0,0 +1,6 @@
+
+//first code 20151125
+
+//take previous code, multiply by 252533, then keep remainder after dividing by 33554393
+
+//Enter the code at row 3010, column 3019. that means i'll have to build a bigger one (compute)
